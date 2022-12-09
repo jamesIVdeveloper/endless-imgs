50f1d49e88d7acb189baf779b044870ee21235cd
James Murphy <jamesivdeveloper@gmail.com>
2022-12-08 21:29:56 -0600

    fetch photos from API, display photos, add load function on scroll

---
+++ script.js
@@ -0,0 +1,75 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photos = [];
+
+// unsplash api
+const count = 30;
+const apiKey = config.API_KEY;
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photos.length;
+
+  photos.forEach((photo) => {
+    const item = document.createElement("a");
+
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    const img = document.createElement("img");
+
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    img.addEventListener("load", imageLoaded);
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photos = await response.json();
+    displayPhotos();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+getPhotos();
